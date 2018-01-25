a41f7a2bd05f168cdcd86dfc8c4756651e3c8f79
pramod-sj <pramodsinghjantwal@gmail.com>
2018-01-25 21:06:11 +0530

    Added texview animation in splash screen

--- app/src/main/java/com/example/pramod/taskplace/SplashScreen.java
+++ app/src/main/java/com/example/pramod/taskplace/SplashScreen.java
@@ -7,7 +7,10 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 /**
  * Created by pramod on 24/1/18.
@@ -17,6 +20,9 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle s){
         super.onCreate(s);
         setContentView(R.layout.splashscreen);
+        TextView t=findViewById(R.id.animtextView);
+        Animation a= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.splashtextanim);
+        t.startAnimation(a);
         final ProgressBar progressBar=findViewById(R.id.progressBar2);
         progressBar.getIndeterminateDrawable().setColorFilter(Color.parseColor("#FF8C00"), PorterDuff.Mode.MULTIPLY);
         new Handler().postDelayed(new Runnable() {
