4dafe80e76e3dcfe6a09c06ee585ccba404be8a8
pramod-sj <pramodsinghjantwal@gmail.com>
2018-02-06 20:49:52 +0530

    removed all geofence stuff

---
+++ app/src/main/java/com/example/pramod/taskplace/Activities/MapsActivity.java
@@ -0,0 +1,34 @@
+package com.example.pramod.taskplace;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,View.OnLongClickListener{
+    private GoogleMap mMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        return false;
+    }
+}
